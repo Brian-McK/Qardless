1a60334300c605505a978c46a47f810a46fc1694
Brian McKenna <D00197352@student.dkit.ie>
2023-01-12 15:45:15 +0000

    adjust styles to login components

--- Components/Login/Login.js
+++ Components/Login/Login.js
@@ -1,28 +1,89 @@
-import { StyleSheet, View, TouchableWithoutFeedback, Keyboard } from "react-native";
+import {
+  StyleSheet,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import { useState, useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { ProgressBar, MD3Colors, Text, Button } from "react-native-paper";
+import { ProgressBar, MD3Colors, Text, Button, TextInput } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useSelector, useDispatch } from "react-redux";
 
 export default function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const submitFormData = async () => {
+    dispatch(
+      getStep3FormData({
+        eircode,
+        phone,
+        password,
+      })
+    );
+
+    const userLoginPayload = {
+      email: step1FormData.email,
+      // email verfied and password hash need to be fixed - TODO
+      emailVerified: true,
+      passwordHash: step3FormData.password,
+    };
+
+    const isFalsy = Object.values(userLoginPayload).some((value) => {
+      if (!value) {
+        return true;
+      }
+      return false;
+    });
+
+    if (isFalsy) {
+      return;
+    }
+
+    registerEndUser(userRegisterPayload);
+
+    console.log("!!", isLoading, isError, isSuccess, isUninitialized, error);
+  };
+
 
   useEffect(() => {
-    console.log("Login screen loaded")
+    console.log("Login screen loaded");
   }, []);
 
   return (
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.displayHeading} variant="headlineMedium">
-          Login
-        </Text>
-      </SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.displayHeading} variant="headlineMedium">
+        Login
+      </Text>
+      <TextInput
+        style={styles.inputField}
+        mode="outlined"
+        label="Email"
+        value={email}
+        onChangeText={(email) => setEmail(email)}
+      />
+      <TextInput
+        style={styles.inputField}
+        mode="outlined"
+        label="Password"
+        value={password}
+        onChangeText={(password) => setPassword(password)}
+      />
+      <Button
+        style={styles.button}
+        mode="contained"
+        onPress={() => submitFormData()}
+      >
+        Submit
+      </Button>
+    </SafeAreaView>
   );
 }
 
 const defaultMargin = 20;
 
-const defaultPadding = 20;
+const defaultPadding = 40;
 
 const styles = StyleSheet.create({
   container: {
@@ -32,6 +93,7 @@ const styles = StyleSheet.create({
   },
   displayHeading: {
     textAlign: "center",
+    marginBottom: defaultMargin
   },
   inputField: {
     marginBottom: defaultMargin,
