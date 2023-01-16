7c5482b6e1541b03c05d8fbfec59a4b565aefee6
Brian McKenna <D00197352@student.dkit.ie>
2023-01-16 18:14:36 +0000

    Added drawer section with drawer items to create the sidenav

--- Components/SideMenu/SideNav.js
+++ Components/SideMenu/SideNav.js
@@ -1,12 +1,18 @@
 import { StyleSheet } from "react-native";
 import { useState, useEffect } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Text } from "react-native-paper";
+import { Text, Drawer } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useSelector, useDispatch } from "react-redux";
 
 export default function SideNav({ navigation }) {
-  const navItems = ["Home", "Certificates", "QR Scanner"];
+  const [active, setActive] = useState("");
+
+  const navItems = [
+    { name: "Home", iconName: "home" },
+    { name: "Certificates", iconName: "clipboard-list" },
+    { name: "QR Scanner", iconName: "barcode" },
+  ];
 
   useEffect(() => {
     console.log("Dashboard screen loaded");
@@ -14,9 +20,18 @@ export default function SideNav({ navigation }) {
 
   return (
     <SafeAreaView style={styles.container}>
-      {navItems.map((item) => (
-        <Text key={item}>{item}</Text>
-      ))}
+      <Drawer.Section title="Menu" showDivider={true}>
+        {navItems.map((item) => (
+          <Drawer.Item
+            style={styles.drawerItems}
+            key={item.name}
+            icon={item.iconName}
+            label={item.name}
+            active={active === item.name}
+            onPress={() => setActive(item.name)}
+          />
+        ))}
+      </Drawer.Section>
     </SafeAreaView>
   );
 }
@@ -28,8 +43,12 @@ const defaultPadding = 20;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "grey",
     padding: defaultPadding,
+    width: "75%",
+  },
+  drawerItems: {
+    color: "red",
   },
   displayHeading: {
     textAlign: "center",
