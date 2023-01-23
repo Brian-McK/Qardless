79c8b6b6fd6296f8b8fa420a259af9e4d9a423ca
Brian McKenna <D00197352@student.dkit.ie>
2023-01-23 13:24:39 +0000

    added re-animated library to help with hand gestures of opening the menu

--- Components/Dashboard/DashboardHome.js
+++ Components/Dashboard/DashboardHome.js
@@ -1,59 +1,55 @@
 import {
-    StyleSheet,
-    View,
-    TouchableWithoutFeedback,
-    Keyboard,
-  } from "react-native";
-  import { useState, useEffect } from "react";
-  import { createNativeStackNavigator } from "@react-navigation/native-stack";
-  import {
-    ProgressBar,
-    MD3Colors,
-    Text,
-    Button,
-    TextInput,
-  } from "react-native-paper";
-  import { SafeAreaView } from "react-native-safe-area-context";
-  import { useSelector, useDispatch } from "react-redux";
-  import SideNav from "../SideMenu/SideNav";
-  
-  export default function DashboardHome({ navigation }) {
-  
-    useEffect(() => {
-      console.log("Dashboard screen loaded");
-    }, []);
-  
-    return (
-      <TouchableWithoutFeedback
-        onPress={() => Keyboard.dismiss()}
-        accessible={false}
-      >
-        <SafeAreaView style={styles.container}>
-          <Text style={styles.displayHeading} variant="headlineMedium">
-            Dashboard Home
-          </Text>
-          <SideNav/>
-        </SafeAreaView>
-      </TouchableWithoutFeedback>
-    );
-  }
-  
-  const defaultMargin = 20;
-  
-  const defaultPadding = 20;
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      padding: defaultPadding,
-    },
-    displayHeading: {
-      textAlign: "center",
-      marginBottom: defaultMargin,
-    },
-    button: {
-      marginBottom: defaultMargin,
-    },
-  });
-  
+  StyleSheet,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { useState, useEffect } from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  ProgressBar,
+  MD3Colors,
+  Text,
+  Button,
+  TextInput,
+} from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useSelector, useDispatch } from "react-redux";
+import SideNav from "../SideMenu/SideNav";
+import RootNavigator from "../SideMenu/RootNavigator";
+
+export default function DashboardHome({ navigation }) {
+  useEffect(() => {
+    console.log("Dashboard screen loaded");
+  }, []);
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => Keyboard.dismiss()}
+      accessible={false}
+    >
+      <SafeAreaView style={styles.container}>
+        <RootNavigator />
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const defaultMargin = 20;
+
+const defaultPadding = 20;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: defaultPadding,
+  },
+  displayHeading: {
+    textAlign: "center",
+    marginBottom: defaultMargin,
+  },
+  button: {
+    marginBottom: defaultMargin,
+  },
+});
--- Components/SideMenu/RootNavigator.js
+++ Components/SideMenu/RootNavigator.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Text, View } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import Certificates from "../Dashboard/Certificates";
+import DrawerContent from "./DrawerContent";
 
 const Drawer = createDrawerNavigator();
 
 export default function RootNavigator() {
   return (
     <Drawer.Navigator drawerContent={() => <DrawerContent />}>
-      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Certificates" component={Certificates} />
     </Drawer.Navigator>
   );
 }
