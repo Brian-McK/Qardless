ff939d2968b767f54abaac8a8d257638a4bf2209
Brian McKenna <D00197352@student.dkit.ie>
2023-02-01 20:56:28 +0000

    update baseUrl in usersApiSlice

--- Redux/api/usersApiSlice.js
+++ Redux/api/usersApiSlice.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const usersApiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://9e3d-64-43-50-159.eu.ngrok.io/api",
+    baseUrl: "https://qardlessapi.azurewebsites.net/api/",
   }),
   tagTypes: ["Enduser"],
   endpoints: (builder) => ({
